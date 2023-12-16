bdbd36c5424e93d1f192a0f21c1402750831c18a
ceugster <christian.eugster@gmx.net>
2023-12-16 09:25:57 +0100

    Update

---
+++ fsl-outlook/src/main/java/ch/eugster/filemaker/fsl/outlook/Client.java
@@ -0,0 +1,15 @@
+package ch.eugster.filemaker.fsl.outlook;
+
+public class Client 
+{
+//	public String connect(String request)
+//	{
+//		IAuthenticationProvider authProvider = new TokenCredentialAuthProvider();
+//		GraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider(authProvider).buildClient();
+//	
+//		User user = graphClient.users("87d349ed-44d7-43e1-9a83-5f2406dee5bd")
+//			.buildRequest()
+//			.get();
+//		return "";
+//	}
+}
